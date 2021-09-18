383b614d7835aeff7f25931b29b97d2b84c99583
JohnGarzon <jhgarzonmedina@hotmail.com>
2021-09-17 20:43:59 -0500

    creamo la carpeta public y los archivos para darle front a la app

--- models/server.js
+++ models/server.js
@@ -3,12 +3,19 @@ const express = require('express');
 class Server {
     constructor() {
         this.app = express();
+        // middlewares
+        this.middlewares();
+        // Rutas 
         this.routes();
         this.listen();
     }
 
+    middlewares() {
+        this.app.use(express.static('public'));
+    }
+
     routes() {
-        this.app.get('/', (req, res) => {
+        this.app.get('/api', (req, res) => {
             res.send('Hola, estamos en el server.js ;)');
         })
     }
